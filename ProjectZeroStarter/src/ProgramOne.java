4c80cedfbe8e726712c0a7afd12947990196a639
TChydro <33527113+TChydro@users.noreply.github.com>
2018-02-06 12:19:41 -0600

    Add files via upload

---
+++ ProjectZeroStarter/src/ProgramOne.java
@@ -0,0 +1,18 @@
+package Starter;
+
+public class ProgramOne
+{
+	//the syntax tree representation of:  one <-- 30 ; echo(one)
+	//You should create separate programs for each tree you create.
+  	private static Stmt programOne = new Stmts( new AssignStmt( "one" , new NumExp(30) ) , new Stmts( new PrintStmt( new LastExpList(new StringExp("one") ) ) ) ) ;
+
+	public static void main(String[] args) 
+	{
+		//Create a new Interpreter Object
+	    Interpreter interpreter = new Interpreter();
+	    System.out.println("Evaluating...");
+	    //Call the overloaded interpret method with the
+	    //static program created above. Should print out 34.
+	    interpreter.interpret(programOne);
+	}
+}
